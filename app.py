f5720b2dd787e27a27fc8c6d0fc3c3d30d29ef32
alfin-cs <alfinsathesh@gmail.com>
2025-10-06 20:06:18 +0300

    installed all the moidule

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
+ 
 @app.route("/", methods=["GET", "POST"])
 def index():
     mood = None
